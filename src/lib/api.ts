9c0f7a87dd732affbd605603ab8460a187913319
Jorbi Nogales <jorbinogales@gmail.com>
2026-02-06 21:30:14 -0400

    feat:environments

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,4 @@
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
-console.log('HOME API Base URL:', API_BASE_URL); // Debugging
 
 interface RequestOptions extends RequestInit {
     token?: string;
